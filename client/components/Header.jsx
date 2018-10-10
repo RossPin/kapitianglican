9a08beaa8a0863c33cabdc1e81d811ec8bb90920
RossPin <dorpetz@hotmail.com>
2018-10-11 12:57:08 +1300

    remove website subheading

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -6,8 +6,7 @@ function Header(props) {
   return (
     <div id="header">
       <div id="logo">
-        <h1><Link to="/">Anglican Parish of Kapiti</Link></h1>
-        <h2>Website</h2>
+        <h1><Link to="/">Anglican Parish of Kapiti</Link></h1>        
       </div>    
     </div>
   )
