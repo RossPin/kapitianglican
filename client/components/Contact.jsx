a0ddf932ea9ac3f5fe062c79005eb5a6b5469ab5
RossPin <dorpetz@hotmail.com>
2018-10-10 17:50:04 +1300

    copy content for components from existing site

---
+++ client/components/Contact.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Contact(props) {
+  
+  return (
+    <div className='content'>
+      <div className="post">
+        <h2 className="title">Click on the link below for a full list of parish staff and contacts.</h2>
+        
+        <div className="entry">
+          <p><a href="http://www.kapitianglican.org.nz/parishdirectory.pdf"> Parish Staff and Contacts PDF</a>
+
+        </p></div>
+      </div>
+    </div>
+  )
+}
+
+export default Contact
---
+++ client/components/Parish.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+function Parish(props) {
+  
+  return (
+    <div className='content'>
+      <div className="post">
+        <h2 className="title">Parish Groups</h2>
+        <p className="byline"></p>
+        <div className="entry">
+          <p>There are many groups operating within the parish on a regular basis and there are many ways for people to be able to contribute to the mission and the ministry of the parish, the giving of 'Talents, Time and Treasure'.</p>
+          <p><strong>"Everyone has a place</strong> <strong>and a ministry"</strong></p>
+          <p>The groups reflect a multigenerational, multicultural parish. All people of all age groups are important and essential to the life of the parish. The nurturing of our young people is a responsibility of the greatest importance.</p>
+          <p><a href="http://www.kapitianglican.org.nz/parishgroups.pdf">Parish Groups PDF</a><a href="http://www.kapitianglican.org.nz/Download.zip"></a></p>
+          <p><a href="http://www.kapitianglican.org.nz/playgroupbrochure.pdf">Preschool music and playgroup PDF</a></p>
+          <p><a href="http://www.kapitianglican.org.nz/messychurch.pdf">Messy Church: What is it? PDF</a></p>
+          <p><a href="https://www.facebook.com/messy.churchkapiti?fref=ts">Messy Church (Facebook)</a></p>
+          <p><a href="https://www.facebook.com/groups/111079862270162/">Kapiti Anglican Youth (facebook)</a></p>
+        </div>
+        <div className="meta">
+          <p className="links">&nbsp;</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Parish
