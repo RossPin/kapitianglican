ce453d6a73b9cb430e091238cf527aa7324dde0d
RossPin <dorpetz@hotmail.com>
2018-10-10 19:05:49 +1300

    remove dead link

--- client/components/Groups.jsx
+++ client/components/Groups.jsx
@@ -11,7 +11,7 @@ function Groups(props) {
           <p>There are many groups operating within the parish on a regular basis and there are many ways for people to be able to contribute to the mission and the ministry of the parish, the giving of 'Talents, Time and Treasure'.</p>
           <p><strong>"Everyone has a place</strong> <strong>and a ministry"</strong></p>
           <p>The groups reflect a multigenerational, multicultural parish. All people of all age groups are important and essential to the life of the parish. The nurturing of our young people is a responsibility of the greatest importance.</p>
-          <p><a href="http://www.kapitianglican.org.nz/parishgroups.pdf" target='_blank'>Parish Groups PDF</a><a href="http://www.kapitianglican.org.nz/Download.zip" target='_blank'>Zip</a></p>
+          <p><a href="http://www.kapitianglican.org.nz/parishgroups.pdf" target='_blank'>Parish Groups PDF</a></p>
           <p><a href="http://www.kapitianglican.org.nz/playgroupbrochure.pdf" target='_blank'>Preschool music and playgroup PDF</a></p>
           <p><a href="http://www.kapitianglican.org.nz/messychurch.pdf" target='_blank'>Messy Church: What is it? PDF</a></p>
           <p><a href="https://www.facebook.com/messy.churchkapiti?fref=ts" target='_blank'>Messy Church (Facebook)</a></p>
