5ff9a0e89305339bafea3809b542400c2b1ee6a4
RossPin <dorpetz@hotmail.com>
2018-10-10 18:02:45 +1300

    remove additional wrapping list

--- client/components/Sidebar.jsx
+++ client/components/Sidebar.jsx
@@ -4,35 +4,28 @@ function Sidebar(props) {
   
   return (
     <div id="sidebar">
+      <h2>Fast Links</h2>
       <ul>
-        <li>
-          <h2>Fast Links</h2>
-          <ul>
-            <li><a href="http://www.kapitianglican.org.nz/sundayservices.pdf">Sunday Services PDF</a></li>
-            <li><a href="http://www.kapitianglican.org.nz/messychurch.pdf">Messy Church: What is it? PDF</a></li>
-            <li><a href="http://www.kapitianglican.org.nz/weeklynewssheet.pdf">News Sheet PDF</a></li>
-            <li><a href="http://www.kapitianglican.org.nz/parishgroups.pdf">Parish Groups PDF</a></li>
-            <li><a href="http://www.kapitianglican.org.nz/playgroupbrochure.pdf">Preschool music and playgroup PDF</a></li>
-            <li><a href="https://www.facebook.com/messy.churchkapiti?fref=ts">Messy Church (Facebook)</a></li>
-            <li><a href="https://www.facebook.com/groups/111079862270162/">Kapiti Anglican Youth (KAYG)</a></li>
-            <li><a href="http://www.kapitianglican.org.nz/contact.pdf">Parish Magazine (Contact) PDF</a></li>
-            <li><a href="http://www.kapitianglican.org.nz/parishdirectory.pdf"> Parish Staff and Contacts PDF</a></li>
-          </ul>
-        
-          <h2>Additional Links</h2>
-          <ul>
-            <li><a href="http://anglicantaonga.org.nz/">Anglican Taonga</a></li>
-            <li><a href="http://wn.anglican.org.nz/">Anglican Diocese of Wellington</a></li>
-            <li><a href="http://anglicandownunder.blogspot.co.nz/">Anglicans Down Under blog</a></li>
-            <li><a href="http://cursillo.org.nz/">Wellington Cursillo</a></li>
-            <li><a href="http://liturgy.co.nz/">Liturgy</a></li>
-            <li><a href="http://anglicancentreinrome.org/">The Anglican Centre in Rome</a></li>
-            <li><a href="http://anglicancommunion.org/">The Anglican Communion</a></li> 
-          </ul>
-        </li>
-        <li>
-          
-        </li>
+        <li><a href="http://www.kapitianglican.org.nz/sundayservices.pdf">Sunday Services PDF</a></li>
+        <li><a href="http://www.kapitianglican.org.nz/messychurch.pdf">Messy Church: What is it? PDF</a></li>
+        <li><a href="http://www.kapitianglican.org.nz/weeklynewssheet.pdf">News Sheet PDF</a></li>
+        <li><a href="http://www.kapitianglican.org.nz/parishgroups.pdf">Parish Groups PDF</a></li>
+        <li><a href="http://www.kapitianglican.org.nz/playgroupbrochure.pdf">Preschool music and playgroup PDF</a></li>
+        <li><a href="https://www.facebook.com/messy.churchkapiti?fref=ts">Messy Church (Facebook)</a></li>
+        <li><a href="https://www.facebook.com/groups/111079862270162/">Kapiti Anglican Youth (KAYG)</a></li>
+        <li><a href="http://www.kapitianglican.org.nz/contact.pdf">Parish Magazine (Contact) PDF</a></li>
+        <li><a href="http://www.kapitianglican.org.nz/parishdirectory.pdf"> Parish Staff and Contacts PDF</a></li>
+      </ul>
+    
+      <h2>Additional Links</h2>
+      <ul>
+        <li><a href="http://anglicantaonga.org.nz/">Anglican Taonga</a></li>
+        <li><a href="http://wn.anglican.org.nz/">Anglican Diocese of Wellington</a></li>
+        <li><a href="http://anglicandownunder.blogspot.co.nz/">Anglicans Down Under blog</a></li>
+        <li><a href="http://cursillo.org.nz/">Wellington Cursillo</a></li>
+        <li><a href="http://liturgy.co.nz/">Liturgy</a></li>
+        <li><a href="http://anglicancentreinrome.org/">The Anglican Centre in Rome</a></li>
+        <li><a href="http://anglicancommunion.org/">The Anglican Communion</a></li> 
       </ul>
     </div>
   )
