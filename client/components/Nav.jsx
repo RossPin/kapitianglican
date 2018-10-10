09959aea1a1c5ae5383291b73c63271a1ed8e229
RossPin <dorpetz@hotmail.com>
2018-10-10 16:05:29 +1300

    fill with links from existing site

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -6,27 +6,25 @@ import { logoutUser } from '../actions/logout'
 function Nav(props) {
   
   return (
-    <div className="nav">
-    {props.auth.isAuthenticated
-      ? //display if logged in
-      <div className="navbar-start">
-        <h1>React App</h1>
-        <div className="navbar-end">
-          <span>{props.auth.user.username} </span>
-          <Link className='button' to="/">Home</Link>
-          <button className='button' onClick={() => props.dispatch(logoutUser())}>Logout</button>
-        </div>
-      </div>
-      : //display if not logged in
-      <div className="navbar-start">
-        <h1>React App</h1>
-        <div className="navbar-end">
-          <Link className='button' to="/login">Login</Link>
-          <Link className='button' to="/">Home</Link>
-          <Link className='button' to="/register">Register</Link>
-        </div>
-      </div>     
-    }
+    <div id="menu">
+      <ul>
+        <li className="active"><a href="http://www.kapitianglican.org.nz/#">Home</a></li>
+        <li><a href="http://www.kapitianglican.org.nz/Sunday%20Services.html"><span style={{color: '#F4914E', fontWeight: 'bold'}}>Sunday Services</span></a>
+        </li><li><a href="http://www.kapitianglican.org.nz/Values.html"><span style={{color: '#F4914E', fontWeight: 'bold'}}>Parish Values</span></a>
+          <ul>
+            <ul>
+              <li><a href="http://www.kapitianglican.org.nz/Parish%20Groups.html"><span style={{color: '#F4914E', fontWeight: 'bold'}}>Parish Groups</span></a></li>
+            </ul>
+          </ul>
+        </li>
+        <li><a href="http://www.kapitianglican.org.nz/Contact.html"><span style={{color: '#F4914E', fontWeight: 'bold'}}>Contacting us</span></a></li>
+        {props.auth.isAuthenticated
+        ?
+        <li><Link to="/login"><span onClick={() => props.dispatch(logoutUser())} style={{color: '#F4914E', fontWeight: 'bold'}}>Logout</span></Link></li>
+        :
+        <li><Link to="/login"><span style={{color: '#F4914E', fontWeight: 'bold'}}>Login</span></Link></li>
+        }
+      </ul>    
     </div>
   )
 }
