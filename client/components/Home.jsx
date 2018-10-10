1c64aabfcfc9d06b8a1e625580641d9d2582f596
RossPin <dorpetz@hotmail.com>
2018-10-10 17:29:31 +1300

    remove redundant links

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 function Home(props) {
   
   return (
-    <div>
+    <div className='content'>
       <div className="post">
-        <h2 className="title"><a href="http://www.kapitianglican.org.nz/#">About Us</a></h2>
+        <h2 className="title">About Us</h2>
         
         <div className="entry">
           <p>We are a growing, mission shaped, multigenerational, multicultural  parish that is open to a mixture of expressions of being Church. We aim to  bring Christ into the community and the community to Christ. The love of  Christ comes before all else. We strive to reflect a life style that is  distinctive, counter cultural, authentic and attractive.</p>
@@ -16,7 +16,7 @@ function Home(props) {
         </div>
       </div>
       <div className="post">
-        <h2 className="title"><a href="http://www.kapitianglican.org.nz/#">Weekly News PDF</a></h2>
+        <h2 className="title">Weekly News PDF</h2>
         
         <div className="entry">Weekly News sheet with  Sunday lectionary readings, parish diary and notices. <a href="http://www.kapitianglican.org.nz/weeklynewssheet.pdf">Latest News sheet</a></div>
     <a href="http://www.kapitianglican.org.nz/sundayservices.pdf">Sunday Services PDF</a>
@@ -25,7 +25,7 @@ function Home(props) {
         </div>
       </div>
       <div className="post">
-        <h2 className="title"><a href="http://www.kapitianglican.org.nz/#">Special Notices</a></h2>
+        <h2 className="title">Special Notices</h2>
         <div className="entry"><iframe width="640" height="360" src="https://youtu.be/vyUdclr6Kzo" frameBorder="0" allowFullScreen=""></iframe></div>
         <div className="meta"> 
     </div>
