ced2de93d9ac12add6dd437e8ab143ec25121033
RossPin <dorpetz@hotmail.com>
2018-10-11 16:28:47 +1300

    change class to className

--- client/components/Services.jsx
+++ client/components/Services.jsx
@@ -4,9 +4,9 @@ function Services(props) {
   
   return (
     <div className='content'>
-      <div class="post">
-        <h2 class="title">Sunday Services</h2>          
-        <div class="entry">
+      <div className="post">
+        <h2 className="title">Sunday Services</h2>          
+        <div className="entry">
           <p>The services and their locations listed below are those  which we regularly have on Sundays. The exception to this is when there is a  month with a 5th Sunday where the parish combines at St Paul’s to  have one service of worship. Prayer lies at the heart of the parish life. It  holds everything together and it sustains every other activity. Faith is centred on Christ and the Gospel. Without Christ nothing makes sense; with Christ all things are possible.</p>
           <p><a href="http://www.kapitianglican.org.nz/sundayservices.pdf" target='_blank'>SundayServices PDF</a></p>
         </div>         
