f938543f2b8b38dec049da6fb1d6e9596c4b2d04
RossPin <dorpetz@hotmail.com>
2018-10-11 15:38:28 +1300

    links list formating consistent

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -10,11 +10,16 @@ class Nav extends React.Component {
       collapsed: true
     }
     this.toggle = this.toggle.bind(this)
+    this.collapse = this.collapse.bind(this)
   }
 
   toggle () {
     let collapsed = this.state.collapsed
     this.setState({collapsed: !collapsed})
+  }
+
+  collapse () {    
+    this.setState({collapsed: true})
   }
 
   render () {
@@ -24,17 +29,17 @@ class Nav extends React.Component {
       <div id='collapse' onClick={this.toggle}>{this.state.collapsed ? '=' : 'X'}</div>
       <div id='menuList'>
       <ul className={this.state.collapsed ? '' : 'show'}>
-        <li className={`${path === '/' ? 'active' : ''}`} onClick={this.toggle}><Link to="/"><span>Home</span></Link></li>
-        <li className={`${path === '/Services' ? 'active' : ''}`} onClick={this.toggle}><Link to="/Services"><span>Sunday Services</span></Link></li>
-        <li className={`${path === '/Values' ? 'active' : ''}`} onClick={this.toggle}><Link to="/Values"><span>Parish Values</span></Link></li>         
-        <li className={`${path === '/Groups' ? 'active' : ''}`} onClick={this.toggle}><Link to="/Groups"><span>Parish Groups</span></Link></li>         
-        <li className={`${path === '/Contact' ? 'active' : ''}`} onClick={this.toggle}><Link to="/Contact"><span>Contacting us</span></Link></li>
-        {!this.state.collapsed && <li className={`${path === '/Contact' ? 'active' : ''}`} onClick={this.toggle}><Link to="/Links"><span>Links</span></Link></li>}
+        <li className={`${path === '/' ? 'active' : ''}`} onClick={this.collapse}><Link to="/"><span>Home</span></Link></li>
+        <li className={`${path === '/Services' ? 'active' : ''}`} onClick={this.collapse}><Link to="/Services"><span>Sunday Services</span></Link></li>
+        <li className={`${path === '/Values' ? 'active' : ''}`} onClick={this.collapse}><Link to="/Values"><span>Parish Values</span></Link></li>         
+        <li className={`${path === '/Groups' ? 'active' : ''}`} onClick={this.collapse}><Link to="/Groups"><span>Parish Groups</span></Link></li>         
+        <li className={`${path === '/Contact' ? 'active' : ''}`} onClick={this.collapse}><Link to="/Contact"><span>Contacting us</span></Link></li>
+        {!this.state.collapsed && <li className={`${path === '/Contact' ? 'active' : ''}`} onClick={this.collapse}><Link to="/Links"><span>Links</span></Link></li>}
         {this.props.auth.isAuthenticated
         ?
-        <li><Link to="/" onClick={this.toggle}><span onClick={() => this.props.dispatch(logoutUser())}>Logout</span></Link></li>
+        <li><Link to="/" onClick={this.collapse}><span onClick={() => this.props.dispatch(logoutUser())}>Logout</span></Link></li>
         :
-        <li className={`${path === '/login' ? 'active' : ''}`} onClick={this.toggle}><Link to="/login"><span>Login</span></Link></li>
+        <li className={`${path === '/login' ? 'active' : ''}`} onClick={this.collapse}><Link to="/login"><span>Login</span></Link></li>
         }
       </ul>
       </div>    
