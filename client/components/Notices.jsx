523196fee6f9831a5b35248954f1febcf7eea230
RossPin <dorpetz@hotmail.com>
2018-10-12 16:50:07 +1300

    neaten up paragraph render code

--- client/components/Notices.jsx
+++ client/components/Notices.jsx
@@ -36,10 +36,8 @@ class Notices extends React.Component {
           {this.state.posts.map(post => (
             <div key={post._id} className="post">
               <h2 className="title">{post.title}{this.props.auth.isAuthenticated && <span> - <button onClick={()=>this.edit(post._id) }>Edit</button> - <button onClick={()=>this.delete(post._id) }>Delete</button></span>}</h2>              
-              <div className="entry">
-                {post.text.split(/[\r\n]{2}/).map((paragraph, i) =>
-                  <p key={i}>{paragraph.split(/[\r\n]/).map((line, i) => <span key={i}>{line}<br /></span>  )}</p>
-                  )}
+              <div className="entry">                
+                <p>{post.text.split(/[\r\n]/).map((line, i) => <span key={i}>{line}<br /></span>)}</p>
               </div>        
             </div>
           ))}        
