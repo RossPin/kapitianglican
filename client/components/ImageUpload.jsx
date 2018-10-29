bc34a9eca64d5722950aa17413bff25a69b13102
RossPin <dorpetz@hotmail.com>
2018-10-29 18:59:48 +1300

    no longer display image

--- client/components/ImageUpload.jsx
+++ client/components/ImageUpload.jsx
@@ -5,8 +5,7 @@ class ImageUpload extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      file: '',
-      fileName: '',   
+      file: '',        
       uploading: false,
       uploaded: false
     }
@@ -14,12 +13,10 @@ class ImageUpload extends React.Component {
 
   handleUpload(e) {
     e.preventDefault()        
-    this.setState({uploading: true})
-    console.log('uploading')
+    this.setState({uploading: true})    
     uploadImage(this.state.file).then(fileName => {
-      this.setState({uploading: false, uploaded: true, fileName})
-      props.setImage(fileName)
-      console.log('uploaded', fileName)
+      this.setState({uploading: false, uploaded: true})
+      this.props.setImage(fileName)      
     })
   }
 
@@ -33,13 +30,13 @@ class ImageUpload extends React.Component {
   }
 
   render() {
-    let {fileName, file, uploading, uploaded} = this.state
+    let {file, uploading, uploaded} = this.state
     return (
       <div >        
           <input type="file" onChange={(e)=>this.handleImageChange(e)} />
           {(file && (!uploaded || uploading)) && <button onClick={(e)=>this.handleUpload(e)}>Upload</button>}
           {uploading && <span>Uploading...</span>}
-          {fileName && <img src={`uploads/${fileName}`} />}
+          
       </div>
     )
   }
