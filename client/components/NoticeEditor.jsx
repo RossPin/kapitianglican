57903b991e47808d6680482124c961d44e9e53b1
RossPin <dorpetz@hotmail.com>
2018-10-31 15:23:34 +1300

    use s3 url for image

--- client/components/NoticeEditor.jsx
+++ client/components/NoticeEditor.jsx
@@ -78,7 +78,7 @@ class NoticeEditor extends React.Component {
           {this.state.image && 
             <div>
             <button onClick={e=>this.deleteImage(e, this.state.image)}>delete image</button>
-            <img src={`uploads/${this.state.image}`} />
+            <img src={`${this.state.image}`} />
             </div>}
           <label>Notice Content</label><br />
           <textarea className="postInputArea" onChange={this.updateDetails} name="text" value={this.state.text}></textarea><br/>               
