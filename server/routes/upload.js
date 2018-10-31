7c6282fe9a8cd4d8d105f4ac69bd003558a4385b
RossPin <dorpetz@hotmail.com>
2018-10-31 16:45:38 +1300

    append local to filename if running localy

--- server/routes/upload.js
+++ server/routes/upload.js
@@ -2,11 +2,11 @@ const router = require('express').Router()
 const aws = require('aws-sdk')
 const S3_BUCKET = process.env.S3_BUCKET
 aws.config.region = 'ap-southeast-2'
+const local = process.env.PORT ? '' : 'local-'
 
 router.delete('/delete', (req, res) => {
   const {fileName} = req.body  
-  if (fileName) deleteFile(fileName, (err, data) => {
-    console.log(data)
+  if (fileName) deleteFile(fileName, (err, data) => {    
     if (err) res.status(400).send({message: err.message})
     else res.status(200).send({message: "File Deleted"})
   })   
@@ -15,7 +15,7 @@ router.delete('/delete', (req, res) => {
 
 router.get('/sign-s3', (req, res) => {
   const s3 = new aws.S3();
-  const fileName = `${Date.now()}-${req.query['file-name']}`;
+  const fileName = `${local+Date.now()}-${req.query['file-name']}`;
   const fileType = req.query['file-type'];
   const s3Params = {
     Bucket: S3_BUCKET,
@@ -26,16 +26,15 @@ router.get('/sign-s3', (req, res) => {
   };
 
   s3.getSignedUrl('putObject', s3Params, (err, data) => {
-    if(err){
-      console.log(err);
-      return res.end();
-    }
-    const returnData = {
-      signedRequest: data,
-      url: `https://${S3_BUCKET}.s3.amazonaws.com/${fileName}`,
-      fileName
-    };
-    res.json(returnData)    
+    if (err) res.status(400).send({message: err.message})
+    else {
+      const returnData = {
+        signedRequest: data,
+        url: `https://${S3_BUCKET}.s3.amazonaws.com/${fileName}`,
+        fileName
+      };
+      res.json(returnData)
+    }    
   });
 });
 
