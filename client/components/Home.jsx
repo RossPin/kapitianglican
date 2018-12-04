b4c5866f432ef3a5b9d5f18662a2c89f96ec12b9
RossPin <dorpetz@hotmail.com>
2018-12-04 13:49:16 +1300

    Basic parallax background

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
-import ParallaxImage from './ParallaxImage'
+import { Parallax } from "react-parallax"
 
 class Home extends React.Component {
     render () {
         return (
             <div>
-                <ParallaxImage />
+                <Parallax bgImage='/images/kapiti.jpg' strength={500}>
+                    <div style={{ height: 400 }}> 
+                        <h1>Our Community</h1>                   
+                    </div>
+                </Parallax>                
                 <div className="about">
                     <h1>Welcome</h1>
                     <p>Kapiti anglican is a community made up of 3 churches from Paraparaumu through to Paekakariki </p>
