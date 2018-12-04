8a497acd7d6c928684d41ec4e2e65d2c65d01a16
RossPin <dorpetz@hotmail.com>
2018-12-04 14:28:16 +1300

    Add images of churches to about

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Parallax } from "react-parallax"
+import { Link } from 'react-router-dom'
 
 class Home extends React.Component {
     render () {
         return (
             <div>
-                <Parallax bgImage='/images/kapiti.jpg' strength={500}>
+                <Parallax bgImage='/images/kapiti.jpg' strength={500} bgWidth={'100%'} >
                     <div style={{ height: 400 }}> 
                         <h1>Our Community</h1>                   
                     </div>
@@ -13,6 +14,11 @@ class Home extends React.Component {
                 <div className="about">
                     <h1>Welcome</h1>
                     <p>Kapiti anglican is a community made up of 3 churches from Paraparaumu through to Paekakariki </p>
+                    <div>
+                        <Link className="parishTile" to={'/stPauls'}><h4>St Paul's</h4><img src="/images/stPauls.jpeg" /></Link>
+                        <Link className="parishTile mid" to={'/stMarks'}><h4>St Mark's</h4><img src="/images/stMarks.jpeg" /></Link>
+                        <Link className="parishTile" to={'/stPeters'}><h4>St Peter's</h4><img src="/images/stPeters.jpeg" /></Link>
+                    </div>
                 </div>
             </div>
         )
