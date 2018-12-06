695ea7cd34d9df0a4a5f59b870baf08f200f2861
RossPin <dorpetz@hotmail.com>
2018-12-07 08:55:56 +1300

    Create Contact component

---
+++ client/components/Contact.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class Contact extends React.Component {
+  
+  render () {    
+    return (
+      <div className='contact'>
+        
+      </div>
+    )
+  }
+}
+
+export default Contact
