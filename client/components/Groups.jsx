719845dc44bab44c34a03841e617a12207513c72
RossPin <dorpetz@hotmail.com>
2018-12-07 08:56:10 +1300

    create groups component

---
+++ client/components/Groups.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class Groups extends React.Component {
+  
+  render () {    
+    return (
+      <div className='groups'>
+        
+      </div>
+    )
+  }
+}
+
+export default Groups
