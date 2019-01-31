a92873a4ebe07eedfd3a49d3444edf050faf9018
RossPin <dorpetz@hotmail.com>
2019-01-31 13:21:53 +1300

    remove dotenv require

--- server/index.js
+++ server/index.js
@@ -1,11 +1,10 @@
-require('dotenv').config()
 const express = require('express')
 const server = express()
 const path = require('path')
 
 server.use(express.static(path.join(__dirname, '../public')))
 
-const PORT = process.env.PORT || 3000
+const PORT = 3000
 
 server.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`)
