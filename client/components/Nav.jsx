555b9a5117348f435f46ffb3080ba7858965e455
RossPin <dorpetz@hotmail.com>
2019-02-08 11:54:14 +1300

    add new menu items

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -29,10 +29,14 @@ class Nav extends React.Component {
         <div >
           <ul className={this.state.collapsed ? '' : 'show'}>
             <li className={`${path === '/' ? 'active' : ''}`} onClick={this.collapse}><Link to='/'><span>Home</span></Link></li>
-            <li className={`${path === '/Services' ? 'active' : ''}`} onClick={this.collapse}><Link to='/Services'><span>Sunday Services</span></Link></li>
-            <li className={`${path === '/Groups' ? 'active' : ''}`} onClick={this.collapse}><Link to='/Groups'><span>Parish Groups</span></Link></li>
-            <li className={`${path === '/Contact' ? 'active' : ''}`} onClick={this.collapse}><Link to='/Contact'><span>Contacting us</span></Link></li>
-            {!this.state.collapsed && <li className={`${path === '/Links' ? 'active' : ''}`} onClick={this.collapse}><Link to='/Links'><span>Links</span></Link></li>}
+            <li className={`${path === '/WhoWeAre' ? 'active' : ''}`} onClick={this.collapse}><Link to='/WhoWeAre'><span>Who We Are</span></Link></li>
+            <li className={`${path === '/WhatWeDo' ? 'active' : ''}`} onClick={this.collapse}><Link to='/WhatWeDo'><span>What We Do</span></Link></li>
+            <li className={`${path === '/PersonalNeeds' ? 'active' : ''}`} onClick={this.collapse}><Link to='/PersonalNeeds'><span>Personal Needs</span></Link></li>
+            <li className={`${path === '/ChurchServices' ? 'active' : ''}`} onClick={this.collapse}><Link to='/ChurchServices'><span>Church Services</span></Link></li>
+            <li className={`${path === '/ParishResources' ? 'active' : ''}`} onClick={this.collapse}><Link to='/ParishResources'><span>Parish Resources</span></Link></li>
+            <li className={`${path === '/Gallery' ? 'active' : ''}`} onClick={this.collapse}><Link to='/Gallery'><span>Gallery</span></Link></li>
+            <li className={`${path === '/FoodCo-op' ? 'active' : ''}`} onClick={this.collapse}><Link to='/FoodCo-op'><span>Food Co-op</span></Link></li>
+            <li className={`${path === '/Contact' ? 'active' : ''}`} onClick={this.collapse}><Link to='/Contact'><span>Contact us</span></Link></li>
           </ul>
         </div>
       </div>
