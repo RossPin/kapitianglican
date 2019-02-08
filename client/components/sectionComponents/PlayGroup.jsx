dde50db897db2c963de89d5f3a2f2ef45e3c2224
RossPin <dorpetz@hotmail.com>
2019-02-08 14:25:05 +1300

    add line above white backgound image

--- client/components/sectionComponents/PlayGroup.jsx
+++ client/components/sectionComponents/PlayGroup.jsx
@@ -4,6 +4,7 @@ class PlayGroup extends React.Component {
   render () {
     return (
       <div className='playGroup'>
+        <div className='line' />
         <img src='images/playGroup.png' className='fullWidth' />
         <h1>Play Group</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quaerimus enim finem bonorum. Pauca mutat vel plura sane; Dempta enim aeternitate nihilo beatior Iuppiter quam Epicurus; Restatis igitur vos; Quodsi ipsam honestatem undique pertectam atque absolutam. Illa tamen simplicia, vestra versuta.</p>
