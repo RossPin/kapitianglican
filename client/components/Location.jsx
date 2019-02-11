b50ec46a546fdcb440113d223a17a39325284158
RossPin <dorpetz@hotmail.com>
2019-02-11 17:35:45 +1300

    show services on location page

--- client/components/Location.jsx
+++ client/components/Location.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 
 const locations = {
   stPauls: {
@@ -7,6 +6,13 @@ const locations = {
     address: [
       '1 Langdale Ave',
       'Paraparaumu'
+    ],
+    services: [
+      <p><strong>8am - </strong>Holy Communion: <ul><li>1st & 3rd Sundays—Book of Common Prayer</li><li>2nd & 4th Sundays—New Zealand Prayer Book</li></ul></p>,
+      <p><strong>9.30am - </strong>Traditional Family service with Holy Communion (with an integrated Children’s programme)</p>,
+      <p><strong>11am - </strong>Family service with Holy Communion</p>,
+      <p><strong>7pm First Sunday of the month - </strong>Stillpoint (A Contemplative service)</p>,
+      <p><strong>4pm Third Sunday of the month - </strong>Messy Church (in the Parish Hall, St Paul’s)</p>
     ],
     search: 'q=st%20pauls%2C%20paraparaumu'
   },
@@ -17,6 +23,10 @@ const locations = {
       'Raumati Beach',
       'Paraparaumu'
     ],
+    services: [
+      <p><strong>9.30am - </strong>Traditional Family service with Holy Communion (with an integrated Children’s programme)</p>,
+      <p><strong>5pm Every second Sunday of the month - </strong>Choral Evensong</p>
+    ],
     search: 'q=st%20marks%2C%20raumati'
   },
   stPeters: {
@@ -24,6 +34,9 @@ const locations = {
     address: [
       'Beach Road',
       'Paekakariki'
+    ],
+    services: [
+      <p><strong>10am - </strong>Traditional Family service with Holy Communion</p>
     ],
     search: 'q=st%20peters%2C%20paekakariki'
   }
@@ -51,18 +64,18 @@ class Location extends React.Component {
     return (
       <div className='location'>
         <div className='line' />
-        <div className='details'>
-          <Link to='/'><h4>BACK</h4></Link>
+        <div className='content'>
           <h1>{locations[location].name}</h1>
-          <img src={`images/${location}.jpeg`} />
-          <h3>Address</h3>
-          <p>{locations[location].address.map((line, i) => <span key={i}>{line}<br /></span>)}</p>
-          <h3>Other Locations</h3>
-          {location !== 'stPauls' && <span><Link to='/location/stPauls'>St Pauls</Link><br /></span>}
-          {location !== 'stMarks' && <span><Link to='/location/stMarks'>St Marks</Link><br /></span>}
-          {location !== 'stPeters' && <span><Link to='/location/stPeters'>St Peters</Link><br /></span>}
+          <div>
+            <img src={`images/${location}.jpeg`} />
+            <div className='mapouter'><div className='gmap_canvas'><iframe id='gmap_canvas' src={`https://maps.google.com/maps?${locations[location].search}&t=&z=15&ie=UTF8&iwloc=&output=embed`} frameBorder='0' scrolling='no' marginHeight='0' marginWidth='0' /></div></div>
+          </div>
+          {locations[location].address.map((line, i) => <span key={i}>{line}<br /></span>)}
+          <a href={`https://maps.google.com/maps?${locations[location].search}`} target='_blank'>Map</a>
+          <h2>Sunday Services</h2>
+          {locations[location].services}
+          <p><strong>Every 5th Sunday of any month there is a Combined Parish Service at St Paul’s at 10am. There are no other services in the Parish on these Sundays.</strong></p>
         </div>
-        <div className='mapouter'><div className='gmap_canvas'><iframe id='gmap_canvas' src={`https://maps.google.com/maps?${locations[location].search}&t=&z=15&ie=UTF8&iwloc=&output=embed`} frameBorder='0' scrolling='no' marginHeight='0' marginWidth='0' /></div></div>
       </div>
     )
   }
