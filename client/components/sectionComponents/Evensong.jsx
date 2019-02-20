0b26ce615c966bd209d99c7f3224f3ccd05efd2e
RossPin <dorpetz@hotmail.com>
2019-02-21 12:43:17 +1300

    add evensong component

---
+++ client/components/sectionComponents/Evensong.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+class Evensong extends React.Component {
+  render () {
+    return (
+      <div className='evensong'>
+        <img src='images/widePlaceholder.jpeg' className='fullWidth' />
+        <div className='content'>
+          <h1>Evensong</h1>
+          <p><strong>5pm Second Sunday of the month - St Marks’s </strong></p>
+          <p><strong>Evensong is:</strong></p>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Evensong
