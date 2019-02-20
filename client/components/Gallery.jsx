7d72108e257d69456f013c273be298399f67385f
RossPin <dorpetz@hotmail.com>
2019-02-21 12:09:33 +1300

    add image lightbox

--- client/components/Gallery.jsx
+++ client/components/Gallery.jsx
@@ -1,31 +1,59 @@
 import React from 'react'
+import Lightbox from 'react-image-lightbox'
+import 'react-image-lightbox/style.css'
 
 const pictures = [
-  { image: 'images/placeholder.jpeg', caption: 'Sample' },
-  { image: 'images/placeholder.jpeg', caption: 'Sample' },
-  { image: 'images/placeholder.jpeg', caption: 'Sample' },
-  { image: 'images/placeholder.jpeg', caption: 'Sample' },
-  { image: 'images/placeholder.jpeg', caption: 'Sample' },
-  { image: 'images/placeholder.jpeg', caption: 'Sample' },
-  { image: 'images/placeholder.jpeg', caption: 'Sample' },
-  { image: 'images/placeholder.jpeg', caption: 'Sample' },
-  { image: 'images/placeholder.jpeg', caption: 'Sample' },
-  { image: 'images/placeholder.jpeg', caption: 'Sample' }
+  'images/placeholder.jpeg',
+  'images/placeholder.jpeg',
+  'images/placeholder.jpeg',
+  'images/placeholder.jpeg',
+  'images/placeholder.jpeg',
+  'images/placeholder.jpeg',
+  'images/placeholder.jpeg',
+  'images/placeholder.jpeg',
+  'images/placeholder.jpeg',
+  'images/placeholder.jpeg'
 ]
 
 class Gallery extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      photoIndex: 0,
+      isOpen: false
+    }
+  }
+
   render () {
+    const { photoIndex, isOpen } = this.state
     return (
       <div className='gallery'>
         <div className='content'>
           <h1>Gallery</h1>
-          {pictures.map(picture => (
-            <div className='tile'>
-              <img src={picture.image} />
-              <h4>{picture.caption}</h4>
+          {pictures.map((picture, i) => (
+            <div className='tile' key={i}>
+              <img src={picture} onClick={() => this.setState({ isOpen: true, photoIndex: i })} />
             </div>
           ))}
         </div>
+        {isOpen && (
+          <Lightbox
+            mainSrc={pictures[photoIndex]}
+            nextSrc={pictures[(photoIndex + 1) % pictures.length]}
+            prevSrc={pictures[(photoIndex + pictures.length - 1) % pictures.length]}
+            onCloseRequest={() => this.setState({ isOpen: false })}
+            onMovePrevRequest={() =>
+              this.setState({
+                photoIndex: (photoIndex + pictures.length - 1) % pictures.length
+              })
+            }
+            onMoveNextRequest={() =>
+              this.setState({
+                photoIndex: (photoIndex + 1) % pictures.length
+              })
+            }
+          />
+        )}
       </div>
     )
   }
