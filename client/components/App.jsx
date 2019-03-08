0f568e61034416405f90bedee531d8d8e3be0783
RossPin <dorpetz@hotmail.com>
2019-03-08 14:22:37 +1300

    position footer as sticky

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -37,36 +37,38 @@ class App extends React.Component {
     return (
       <HashRouter>
         <div>
-          <Route path='/' component={Header} />
-          <Route exact path='/' component={Home} />
-          <Route path='/location/:location' component={Location} />
-          <Route path='/ChurchServices' component={Services} />
-          <Route path='/WhoWeAre' component={WhoWeAre} />
-          <Route path='/WhatWeDo' component={WhatWeDo} />
-          <Route path='/PersonalNeeds' component={PersonalNeeds} />
-          <Route exact path='/ParishResources' component={ParishResources} />
-          <Route path='/ParishResources/Newsletter' component={Newsletter} />
-          <Route path='/ParishResources/Magazine' component={Magazine} />
-          <Route path='/ParishResources/sermons' component={Sermons} />
-          <Route path='/ParishResources/calendar' component={Calendar} />
-          <Route path='/Gallery' component={Gallery} />
-          <Route path='/FoodCo-op' component={FoodCoOp} />
-          <Route path='/Contact' component={Contact} />
-          <div>
-            <Route path='/KeyPeople' component={KeyPeople} />
-            <Route path='/MissionVisionValues' component={MissionVisionValues} />
-            <Route path='/ParishGroups' component={ParishGroups} />
-            <Route path='/PlayGroup' component={PlayGroup} />
-            <Route path='/RestHomeMinistry' component={RestHomeMinistry} />
-            <Route path='/ParishPrayer' component={ParishPrayer} />
-            <Route path='/SpecialEvents' component={SpecialEvents} />
-            <Route path='/Loved4Life' component={Loved4Life} />
-            <Route path='/PrayersSquares' component={PrayersSquares} />
-            <Route path='/FuneralMarriageBaptism' component={FuneralMarriageBaptism} />
-            <Route path='/PastoralCare' component={PastoralCare} />
-            <Route path='/MessyChurch' component={MessyChurch} />
-            <Route path='/Evensong' component={Evensong} />
-            <Route path='/Stillpoint' component={Stillpoint} />
+          <div className='container'>
+            <Route path='/' component={Header} />
+            <Route exact path='/' component={Home} />
+            <Route path='/location/:location' component={Location} />
+            <Route path='/ChurchServices' component={Services} />
+            <Route path='/WhoWeAre' component={WhoWeAre} />
+            <Route path='/WhatWeDo' component={WhatWeDo} />
+            <Route path='/PersonalNeeds' component={PersonalNeeds} />
+            <Route exact path='/ParishResources' component={ParishResources} />
+            <Route path='/ParishResources/Newsletter' component={Newsletter} />
+            <Route path='/ParishResources/Magazine' component={Magazine} />
+            <Route path='/ParishResources/sermons' component={Sermons} />
+            <Route path='/ParishResources/calendar' component={Calendar} />
+            <Route path='/Gallery' component={Gallery} />
+            <Route path='/FoodCo-op' component={FoodCoOp} />
+            <Route path='/Contact' component={Contact} />
+            <div>
+              <Route path='/KeyPeople' component={KeyPeople} />
+              <Route path='/MissionVisionValues' component={MissionVisionValues} />
+              <Route path='/ParishGroups' component={ParishGroups} />
+              <Route path='/PlayGroup' component={PlayGroup} />
+              <Route path='/RestHomeMinistry' component={RestHomeMinistry} />
+              <Route path='/ParishPrayer' component={ParishPrayer} />
+              <Route path='/SpecialEvents' component={SpecialEvents} />
+              <Route path='/Loved4Life' component={Loved4Life} />
+              <Route path='/PrayersSquares' component={PrayersSquares} />
+              <Route path='/FuneralMarriageBaptism' component={FuneralMarriageBaptism} />
+              <Route path='/PastoralCare' component={PastoralCare} />
+              <Route path='/MessyChurch' component={MessyChurch} />
+              <Route path='/Evensong' component={Evensong} />
+              <Route path='/Stillpoint' component={Stillpoint} />
+            </div>
           </div>
           <Footer />
         </div>
