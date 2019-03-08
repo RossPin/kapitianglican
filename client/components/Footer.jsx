f6006a52d4ce38fd68c04f244f2dbf6815c83859
RossPin <dorpetz@hotmail.com>
2019-03-08 14:33:13 +1300

    add calendar link

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 class Footer extends React.Component {
   render () {
     return (
       <div className='footer'>
+        <div className='rightLink'>
+          <Link to='/ParishResources/Calendar'>Calendar</Link>
+        </div>
         <ul>
           <li><a href='https://www.facebook.com/Anglican-Parish-of-Kapiti-112319898809569/' target='_blank'>Kapiti Anglican Facebook Page</a></li>
           <li><a href='http://movementonline.org.nz/' target='_blank'>Movement Online</a></li>
