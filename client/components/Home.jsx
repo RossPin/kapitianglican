6225e94aa731085c46b0166eada15de167dba8f1
RossPin <dorpetz@hotmail.com>
2020-04-26 11:18:09 +1200

    Add covid update from current site

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -13,8 +13,20 @@ class Home extends React.Component {
           </div>
         </Parallax>
         <div className='about'>
-          <h1>Who we are</h1>
-          <p>Kapiti anglican is a community made up of 3 churches from Paraparaumu through to Paekakariki </p>
+          <h2 >Changes to Services During COVID-19</h2>
+          <div >
+            <p>All church services are currently suspended until a return to level 2 on the Coved-19 alert scale. In the meantime we are hosting church@home.</p>
+            <h4>Morning Prayer - 8.30am Mon to Fri</h4>
+            <p><a target='_blank' href='https://us04web.zoom.us/j/703543297'>https://us04web.zoom.us/j/703543297</a> or <a target='_blank' href='https://www.zoom.us/'>https://www.zoom.us</a>. Please click on JOIN A MEETING (top right). The Meeting ID is 703 543 297.</p>
+            <h4>Night prayer - 8pm Mon to Fri</h4>
+            <p><a target='_blank' href='https://us04web.zoom.us/j/162960012'>https://us04web.zoom.us/j/162960012</a> or <a target='_blank' href='https://www.zoom.us/'>https://www.zoom.us</a>. Please click on JOIN A MEETING (top right). The Meeting ID is 162 960 012.</p> 
+            <h4>Short Communion Service- Sunday 8.30am</h4>
+            <p><a target='_blank' href='https://us04web.zoom.us/j/163441912'>https://us04web.zoom.us/j/163441912</a> or <a target='_blank' href='https://www.zoom.us/'>https://www.zoom.us</a>. Please click on JOIN A MEETING (top right). The Meeting ID is 163 441 912.</p> 
+            <h4>Wider Church Online Service - Sunday 10am</h4>
+            <p>Live stream on <a target='_blank' href='https://movementonline.org.nz/'>Anglican Movement</a> or Freeview channel 200.</p>
+          </div>
+          <h2>Who we are</h2>
+          <p>We are a growing, mission shaped, multigenerational, multicultural parish that is open to a mixture of expressions of being Church. We aim to bring Christ into the community and the community to Christ. The love of Christ comes before all else. We strive to reflect a life style that is distinctive, counter cultural, authentic and attractive.</p>
           <div>
             <Link className='parishTile' to={'/location/stPauls'}><h4>St Paul's</h4><img src='images/stPauls.jpeg' /></Link>
             <Link className='parishTile mid' to={'/location/stMarks'}><h4>St Mark's</h4><img src='images/stMarks.jpeg' /></Link>
@@ -23,7 +35,7 @@ class Home extends React.Component {
         </div>
         <Parallax bgImage='images/Raumati-Dune.jpg' strength={500} >
           <div className='parallaxContent' style={{ height: 400 }}>
-            <h1>Notices</h1>
+            <h2>Notices</h2>
           </div>
         </Parallax>
         <Notices history={this.props.history} />
