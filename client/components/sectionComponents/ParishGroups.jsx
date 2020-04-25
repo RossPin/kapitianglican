20735e07d3f9c5e132775ebfd436de7c2e483827
RossPin <dorpetz@hotmail.com>
2020-04-26 11:18:50 +1200

    Remove aditional tag

--- client/components/sectionComponents/ParishGroups.jsx
+++ client/components/sectionComponents/ParishGroups.jsx
@@ -8,7 +8,7 @@ class ParishGroups extends React.Component {
         <div className='content'>
           <h1>Parish Groups</h1>
           <p>There are many groups operating within the parish on a regular basis and there are many ways for people to be able to contribute to the mission and the ministry of the parish, the giving of 'Talents, Time and Treasure'.</p>
-          <p><strong>"Everyone has a place</strong> <strong>and a ministry"</strong></p>
+          <p><strong>"Everyone has a place and a ministry"</strong></p>
           <p>The groups reflect a multigenerational, multicultural parish. All people of all age groups are important and essential to the life of the parish. The nurturing of our young people is a responsibility of the greatest importance.</p>
           <p><a href='http://www.kapitianglican.org.nz/parishgroups.pdf' target='_blank'>Parish Groups PDF</a></p>
           <p><a href='http://www.kapitianglican.org.nz/playgroupbrochure.pdf' target='_blank'>Preschool music and playgroup PDF</a></p>
