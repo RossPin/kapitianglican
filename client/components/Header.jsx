e390690c7d3aae46b550e11a413b4f984cb5f9cc
RossPin <dorpetz@hotmail.com>
2020-08-04 10:08:02 +1200

    New logo

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -5,7 +5,7 @@ class Header extends React.Component {
   render () {
     return (
       <div className='header'>
-        <img className='logo' src='images/logo.png' />
+        <img className='logo' src='images/Kapiti_Anglicans_WordMark_Colour.svg' />
         <Nav location={this.props.location} />
       </div>
     )
