33391861fa3d7c125b4cd725c2eefb48de3ddcc1
RossPin <dorpetz@hotmail.com>
2020-10-04 15:38:29 +1300

    remove redundant components

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -21,8 +21,6 @@ import RestHomeMinistry from './sectionComponents/RestHomeMinistry'
 import ParishPrayer from './sectionComponents/ParishPrayer'
 import SpecialEvents from './sectionComponents/SmallGroups'
 import Loved4Life from './sectionComponents/Loved4Life'
-import PrayersSquares from './sectionComponents/PrayersSquares'
-import FuneralMarriageBaptism from './sectionComponents/FuneralMarriageBaptism'
 import PastoralCare from './sectionComponents/PastoralCare'
 import MessyChurch from './sectionComponents/MessyChurch'
 import Newsletter from './Newsletter'
@@ -63,8 +61,6 @@ class App extends React.Component {
               <Route path='/ParishPrayer' component={ParishPrayer} />
               <Route path='/SpecialEvents' component={SpecialEvents} />
               <Route path='/Loved4Life' component={Loved4Life} />
-              <Route path='/PrayersSquares' component={PrayersSquares} />
-              <Route path='/FuneralMarriageBaptism' component={FuneralMarriageBaptism} />
               <Route path='/PastoralCare' component={PastoralCare} />
               <Route path='/MessyChurch' component={MessyChurch} />
               <Route path='/AlphaCourses' component={AlphaCourses} />
