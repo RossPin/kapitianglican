a554805db12eee26f1bec216e3b3007164678aaa
RossPin <dorpetz@hotmail.com>
2020-10-04 15:34:42 +1300

    link new components

--- client/components/ServingKapiti.jsx
+++ client/components/ServingKapiti.jsx
@@ -2,22 +2,22 @@ import React from 'react'
 import PlayGroup from './sectionComponents/PlayGroup'
 import FoodCoOp from './sectionComponents/FoodCoOp'
 import RestHomeMinistry from './sectionComponents/RestHomeMinistry'
-import ParishPrayer from './sectionComponents/ParishPrayer'
-import SmallGroups from './sectionComponents/SmallGroups'
 import Loved4Life from './sectionComponents/Loved4Life'
-import PrayersSquares from './sectionComponents/PrayersSquares'
+import GamesAfternoon from './sectionComponents/GamesAfternoon'
+import AlphaCourses from './sectionComponents/AlphaCourses'
+import VenueHire from './sectionComponents/VenueHire'
 
 class ServingKapiti extends React.Component {
   render () {
     return (
       <div className='ServingKapiti'>
         <PlayGroup />
+        <Loved4Life />
         <FoodCoOp />
         <RestHomeMinistry />
-        <ParishPrayer />
-        <SmallGroups />
-        <Loved4Life />
-        <PrayersSquares />
+        <GamesAfternoon />
+        <AlphaCourses />
+        <VenueHire />
       </div>
     )
   }
