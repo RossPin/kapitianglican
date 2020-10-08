ba74f92cae575731d5cee25913debd3d082969e9
RossPin <dorpetz@hotmail.com>
2020-10-08 17:01:06 +1300

    redirect to password page

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, withRouter } from 'react-router-dom'
+import { connect } from 'react-redux'
 
 class Nav extends React.Component {
   constructor (props) {
@@ -10,6 +11,19 @@ class Nav extends React.Component {
     this.toggle = this.toggle.bind(this)
     this.collapse = this.collapse.bind(this)
   }
+
+  componentDidMount () {
+    if (this.props.auth.isAuthenticated == false && this.props.location.pathname != '/password') {
+      this.props.history.push('/password')
+    }
+  }
+  
+  componentDidUpdate (prevProps) {
+    if (this.props.auth.isAuthenticated == false && this.props.location.pathname != '/password') {
+      this.props.history.push('/password')
+    }
+  }
+
 
   toggle () {
     let collapsed = this.state.collapsed
@@ -25,58 +39,66 @@ class Nav extends React.Component {
     return (
 
       <div id='menu'>
-        <div id='collapse' onClick={this.toggle}>{this.state.collapsed ? '=' : 'X'}</div>
-        <div >
-          <ul className={this.state.collapsed ? '' : 'show'}>
-            <li className={`${path === '/' ? 'active' : ''}`} onClick={this.collapse}><Link to='/' className='menuLink'><span>Home</span></Link></li>
-            <li className={`${path === '/AboutUs' ? 'active' : ''} dropdown`} onClick={this.collapse}>
-              <Link to='/AboutUs' className='menuLink'><span>About Us</span></Link>
-              <div className='dropdownContent'>
-                <Link to='/WhatWeAreAbout'>What We Are About</Link>
-                <Link to='/WhosWho'>Who's Who</Link>
-                <Link to='/OurChurches'>Our Churches and Congregations</Link>
-                <Link to='/VenueHire'>Venue Hire</Link>
-                <Link to='/Gallery'>Picture Gallery</Link>
-              </div>
-            </li>
-            <li className={`${path === '/WhatsHappening' ? 'active' : ''} dropdown`} onClick={this.collapse}>
-              <Link to='/WhatsHappening' className='menuLink'><span>What's Happening</span></Link>
-              <div className='dropdownContent'>
-                <Link to='/Services'>Service times</Link>                
-                <Link to='/ParishPrayer'>Parish Prayer</Link>
-                <Link to='/SmallGroups'>Small Groups</Link>
-                <Link to='/PastoralCare'>Pastoral Care</Link>
-                <Link to='/WhatsHappening/Sermons'>Sermons</Link>
-                <Link to='/WhatsHappening/Newsletter'>Newsletter</Link>
-                <Link to='/WhatsHappening/Calendar'>Calendar</Link>
-              </div>
-            </li>
-            <li className={`${path === '/ChildrenAndFamilies' ? 'active' : ''} dropdown`} onClick={this.collapse}>
-              <Link to='/ChildrenAndFamilies' className='menuLink'><span>Children and Families</span></Link>
-              <div className='dropdownContent'>
-                <Link to='/Sundays'>Sundays</Link>
-                <Link to='/FourPlusMore'>4+More</Link>
-                <Link to='/SchoolsSupport'>SchoolsSupport</Link>
-                <Link to='/PlayGroup'>Play Group</Link>
-              </div>
-            </li>
-            <li className={`${path === '/ServingKapiti' ? 'active' : ''} dropdown`} onClick={this.collapse}>
-              <Link to='/ServingKapiti' className='menuLink'><span>Serving Kapiti</span></Link>
-              <div className='dropdownContent'>
-                <Link to='/PlayGroup'>Play Group</Link>
-                <Link to='/Loved4Life'>Loved 4 Life</Link>
-                <Link to='/FoodCo-op'>Kapiti Fruit and Vege Coop</Link>
-                <Link to='/RestHomeMinistry'>Rest Home Ministry</Link>
-                <Link to='/GamesAfternoon'>Games Afternoon</Link>
-                <Link to='/ALphaCourses'>Alpha and Alpha Marriage Courses</Link>
-                <Link to='/VenueHire'>Venue Hire</Link>
-              </div>
-            </li>            
-          </ul>
+        {this.props.auth.isAuthenticated &&
+        <div>
+          <div id='collapse' onClick={this.toggle}>{this.state.collapsed ? '=' : 'X'}</div>
+          <div >
+            <ul className={this.state.collapsed ? '' : 'show'}>
+              <li className={`${path === '/' ? 'active' : ''}`} onClick={this.collapse}><Link to='/' className='menuLink'><span>Home</span></Link></li>
+              <li className={`${path === '/AboutUs' ? 'active' : ''} dropdown`} onClick={this.collapse}>
+                <Link to='/AboutUs' className='menuLink'><span>About Us</span></Link>
+                <div className='dropdownContent'>
+                  <Link to='/WhatWeAreAbout'>What We Are About</Link>
+                  <Link to='/WhosWho'>Who's Who</Link>
+                  <Link to='/OurChurches'>Our Churches and Congregations</Link>
+                  <Link to='/VenueHire'>Venue Hire</Link>
+                  <Link to='/Gallery'>Picture Gallery</Link>
+                </div>
+              </li>
+              <li className={`${path === '/WhatsHappening' ? 'active' : ''} dropdown`} onClick={this.collapse}>
+                <Link to='/WhatsHappening' className='menuLink'><span>What's Happening</span></Link>
+                <div className='dropdownContent'>
+                  <Link to='/Services'>Service times</Link>                
+                  <Link to='/ParishPrayer'>Parish Prayer</Link>
+                  <Link to='/SmallGroups'>Small Groups</Link>
+                  <Link to='/PastoralCare'>Pastoral Care</Link>
+                  <Link to='/WhatsHappening/Sermons'>Sermons</Link>
+                  <Link to='/WhatsHappening/Newsletter'>Newsletter</Link>
+                  <Link to='/WhatsHappening/Calendar'>Calendar</Link>
+                </div>
+              </li>
+              <li className={`${path === '/ChildrenAndFamilies' ? 'active' : ''} dropdown`} onClick={this.collapse}>
+                <Link to='/ChildrenAndFamilies' className='menuLink'><span>Children and Families</span></Link>
+                <div className='dropdownContent'>
+                  <Link to='/Sundays'>Sundays</Link>
+                  <Link to='/FourPlusMore'>4+More</Link>
+                  <Link to='/SchoolsSupport'>SchoolsSupport</Link>
+                  <Link to='/PlayGroup'>Play Group</Link>
+                </div>
+              </li>
+              <li className={`${path === '/ServingKapiti' ? 'active' : ''} dropdown`} onClick={this.collapse}>
+                <Link to='/ServingKapiti' className='menuLink'><span>Serving Kapiti</span></Link>
+                <div className='dropdownContent'>
+                  <Link to='/PlayGroup'>Play Group</Link>
+                  <Link to='/Loved4Life'>Loved 4 Life</Link>
+                  <Link to='/FoodCo-op'>Kapiti Fruit and Vege Coop</Link>
+                  <Link to='/RestHomeMinistry'>Rest Home Ministry</Link>
+                  <Link to='/GamesAfternoon'>Games Afternoon</Link>
+                  <Link to='/ALphaCourses'>Alpha and Alpha Marriage Courses</Link>
+                  <Link to='/VenueHire'>Venue Hire</Link>
+                </div>
+              </li>            
+            </ul>
+          </div>
         </div>
+        }
       </div>
     )
   }
 }
 
-export default Nav
+const mapStateToProps = ({ auth }) => {
+  return { auth }
+}
+
+export default withRouter(connect(mapStateToProps)(Nav))
