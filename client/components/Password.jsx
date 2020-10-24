799e6b3dbd73dc5eaa3365b438d1a817fa643ef6
RossPin <dorpetz@hotmail.com>
2020-10-25 10:59:18 +1300

    Password page basic layout

--- client/components/Password.jsx
+++ client/components/Password.jsx
@@ -23,12 +23,19 @@ class Password extends React.Component {
   }
   render() {
     return (
-      <form className="password" onSubmit={this.submit}>
+      <div className="password">
+        <img className='logo' src='images/Kapiti_Anglicans_WordMark_Colour.svg' />
+        <h1>Welcome to Kapiti Anglicans</h1>
+        <h2>This is the new website under development</h2>
+        <h3>Visit our current website at <a href='http://kapitianglican.org.nz'>kapitianglican.org.nz</a>.</h3>
+        <h3>Or enter password to preview the new site.</h3>
+        <form  onSubmit={this.submit}>
           <label>Password:
-          <input style={{margin: '0.5vw'}} type="password" name="password" onChange={this.updateDetails}/>
-        </label><br/>
-        <input className="button" type="submit" />
-      </form>
+            <input style={{margin: '0.5vw'}} type="password" name="password" onChange={this.updateDetails}/>
+          </label><br/>
+          <input className="button" type="submit" />
+        </form>
+      </div>
     )
   }
 }
