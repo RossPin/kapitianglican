f2422a89c3044402c640bdde8e7b7341535881aa
RossPin <dorpetz@hotmail.com>
2020-10-25 16:28:25 +1300

    update SmallGroups

--- client/components/sectionComponents/SmallGroups.jsx
+++ client/components/sectionComponents/SmallGroups.jsx
@@ -7,7 +7,9 @@ class SmallGroups extends React.Component {
         <img src='images/widePlaceholder.jpeg' className='fullWidth' />
         <div className='content'>
           <h2>Small Groups</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quaerimus enim finem bonorum. Pauca mutat vel plura sane; Dempta enim aeternitate nihilo beatior Iuppiter quam Epicurus; Restatis igitur vos; Quodsi ipsam honestatem undique pertectam atque absolutam. Illa tamen simplicia, vestra versuta.</p>
+          <p><strong>Home groups and house churches-</strong> Kapiti Parish boasts a good number of home groups and house churches where parishioners meet in smaller groups for encouragement, prayer, Bible Study and reflection. Most groups meet weekly and we often have extra groups running through Lent. Kathleen Fleck is the co-ordinator. Contact the office for details.</p>
+          <p><strong>Men’s and Women’s Fellowship Groups Marksmen-</strong> a men’s group that meets monthly on the   Monday of the month in the Parish Lounge for a meal and to listen to a speaker on a topical issue. Newcomers are most welcome. Contact office for details.</p>
+          <p><strong>Kapiti Women’s Fellowship-</strong> this group gathers members of the Association of Anglican women and the Mothers union- two organisations pledged to uphold the welfare of women, children, and families of all kinds, for food, fellowship, worship and speakers.  Meetings are usually on the 2nd Thursday of the month in the Parish lounge. Newcomers are welcome. Contact the office for details.</p>
         </div>
       </div>
     )
