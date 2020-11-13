f61aa887ee01c728794cd0016c213085105dec29
RossPin <dorpetz@hotmail.com>
2020-11-14 11:56:05 +1300

    status url

--- server/server.js
+++ server/server.js
@@ -8,6 +8,10 @@ server.use(bodyParser.json())
 server.use('/api/users', users)
 server.use(express.static(path.join(__dirname, '../docs')))
 
+server.get('/status', (req, res) => {
+  res.status(200).send({ status: 'running' })
+})
+
 server.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../docs/index.html'))
 })
