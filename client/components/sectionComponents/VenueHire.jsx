af0e4fc6af69a1f2dae6a8a9c62b915cec795d0f
RossPin <dorpetz@hotmail.com>
2021-03-23 11:46:41 +1300

    Venue hire info

--- client/components/sectionComponents/VenueHire.jsx
+++ client/components/sectionComponents/VenueHire.jsx
@@ -6,7 +6,12 @@ class VenueHire extends React.Component {
       <div className='venueHire'>
         <div className='content'>
           <h2>VenueHire</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quaerimus enim finem bonorum. Pauca mutat vel plura sane; Dempta enim aeternitate nihilo beatior Iuppiter quam Epicurus; Restatis igitur vos; Quodsi ipsam honestatem undique pertectam atque absolutam. Illa tamen simplicia, vestra versuta.</p>
+          <h4>St Mark's - Rita King Cottage</h4>
+          <p>This venue has a large meeting room.  It also has a fully equipped kitchen, fridge, 2 stoves and toilet/bathroom.</p>
+          <h4>St Paul's Church</h4>
+          <p>This venue has the options of Church, lounge or hall.<br />Equipment - The Parish hall and kitchen has enough plates and cutlery for 100 people.  There are 14 tables that can each seat up to 10 people.  The chairs for the hall are plastic and are not padded.  Users of the kitchen are required to dispose of all perishable food.  The kitchen has 2 electric ranges, fridge and microwave.</p>
+          <h4>Policy on alcohol</h4>
+          <p>Permission must be gained from the Vicar for the serving of alcohol on the premises.  Where permission is given, it will only be for wine and beer (which must be in a bottle or can), no keg beer or spirits are allowed.  It is not permissible to sell alcohol in or on any of our church properties.</p>
         </div>
       </div>
     )
