a7bd5a7dc2e852e5be10a7bf89ea5092fded37df
RossPin <dorpetz@hotmail.com>
2021-03-24 11:40:53 +1300

    use withrouter to expose location

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,18 +1,15 @@
 import React from 'react'
 import Nav from './Nav'
+import { withRouter } from 'react-router-dom'
 import { connect } from 'react-redux'
 
 class Header extends React.Component {
-  constructor (props) {
-    super(props)    
-  }
-
   componentDidMount () {
-    if (this.props.auth.isAuthenticated === false && this.props.location && this.props.location.pathname !== '/password') {
+    if (this.props.auth.isAuthenticated === false && this.props.location.pathname !== '/password') {
       this.props.history.push('/password')
     }
   }
-  
+
   componentDidUpdate (prevProps) {
     if (this.props.auth.isAuthenticated === false && this.props.location.pathname !== '/password') {
       this.props.history.push('/password')
@@ -37,4 +34,4 @@ const mapStateToProps = ({ auth }) => {
   return { auth }
 }
 
-export default connect(mapStateToProps)(Header)
+export default withRouter(connect(mapStateToProps)(Header))
