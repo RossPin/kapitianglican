37d8a7a08da4d48af65ab6bd5c23ce5ad9e6dc11
RossPin <dorpetz@hotmail.com>
2021-03-24 10:44:36 +1300

    Swap small slideshow image

--- client/components/Slideshow.jsx
+++ client/components/Slideshow.jsx
@@ -1,30 +1,27 @@
-import React from 'react';
-import { Slide } from 'react-slideshow-image';
+import React from 'react'
+import { Slide } from 'react-slideshow-image'
 const slideImages = [
-  'images/gallery/sample1.jpg',
+  'images/gallery/sample5.jpg',
   'images/gallery/sample2.jpg',
   'images/gallery/sample3.jpg'
-];
- 
+]
+
 const Slideshow = () => {
-    return (
-      <div className="slide-container">
-        <Slide>
-          <div className="each-slide">
-            <div style={{'backgroundImage': `url(${slideImages[0]})`}}>              
-            </div>
-          </div>
-          <div className="each-slide">
-            <div style={{'backgroundImage': `url(${slideImages[1]})`}}>              
-            </div>
-          </div>
-          <div className="each-slide">
-            <div style={{'backgroundImage': `url(${slideImages[2]})`}}>
-            </div>
-          </div>
-        </Slide>
-      </div>
-    )
+  return (
+    <div className='slide-container'>
+      <Slide>
+        <div className='each-slide'>
+          <div style={{ 'backgroundImage': `url(${slideImages[0]})` }} />
+        </div>
+        <div className='each-slide'>
+          <div style={{ 'backgroundImage': `url(${slideImages[1]})` }} />
+        </div>
+        <div className='each-slide'>
+          <div style={{ 'backgroundImage': `url(${slideImages[2]})` }} />
+        </div>
+      </Slide>
+    </div>
+  )
 }
 
 export default Slideshow
